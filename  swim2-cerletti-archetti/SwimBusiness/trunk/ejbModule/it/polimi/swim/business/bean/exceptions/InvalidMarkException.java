43be4a159dde5e049b27e4a397a03f4d64460af2
Chiara <kia90yaya@hotmail.it>
2012-12-20 09:40:44 +0000

    Implement feedback controller

---  swim2-cerletti-archetti/SwimBusiness/trunk/ejbModule/it/polimi/swim/business/bean/exceptions/InvalidMarkException.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.swim.business.bean.exceptions;
-
-public class InvalidMarkException extends Exception {
-
-	private static final long serialVersionUID = 6943003067677629409L;
-
-}
---  swim2-cerletti-archetti/SwimBusiness/trunk/ejbModule/it/polimi/swim/business/bean/remote/FeedbackControllerRemote.java
+++  swim2-cerletti-archetti/SwimBusiness/trunk/ejbModule/it/polimi/swim/business/bean/remote/FeedbackControllerRemote.java
@@ -1,15 +1,18 @@
 package it.polimi.swim.business.bean.remote;
 
-import it.polimi.swim.business.bean.exceptions.InvalidMarkException;
 import it.polimi.swim.business.exceptions.UnauthorizedRequestException;
+import it.polimi.swim.business.exceptions.BadRequestException;
+import it.polimi.swim.business.exceptions.InvalidStateException;
 
 import javax.ejb.Remote;
 
 @Remote
 public interface FeedbackControllerRemote {
 	public void createFeedback(int workRequestId, String authorUsr, int mark,
-			String review) throws UnauthorizedRequestException, InvalidMarkException;
+			String review) throws UnauthorizedRequestException,
+			BadRequestException, InvalidStateException;
 
 	public void replyToFeedback(int workRequestId, String authorUsr,
-			String reply) throws UnauthorizedRequestException;
+			String reply) throws UnauthorizedRequestException,
+			BadRequestException, InvalidStateException;
 }
