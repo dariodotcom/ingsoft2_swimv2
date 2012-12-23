bd01b80300cd50322cc5c22aa24bb89114a50a8d
chiara.cerletti@gmail.com <heartripper@users.noreply.github.com>
2012-12-23 09:19:44 +0000

    Aggiunto JavaDoc

---  swim2-cerletti-archetti/SwimWeb/trunk/src/it/polimi/swim/web/pagesupport/AdminMenu.java
+++  swim2-cerletti-archetti/SwimWeb/trunk/src/it/polimi/swim/web/pagesupport/AdminMenu.java
@@ -1,25 +1,48 @@
 package it.polimi.swim.web.pagesupport;
 
+/**
+ * AdminMenu is an enumeration which implements interface MenuDescriptor in
+ * order to provide informations on the menu displayed to the administrator.
+ */
 public enum AdminMenu implements MenuDescriptor {
 
 	MANAGEMENT("Gestione", "/admin/", "management");
 
 	private String tabName, tabLink, elemId;
 
+	/**
+	 * Constructor method.
+	 * 
+	 * @param tabName
+	 *            a String displayed as the name of the tab.
+	 * @param tabLink
+	 *            a String which identifies the path of the link of the tab.
+	 * @param elemId
+	 *            a String which identifies the HTML identifier of the tab.
+	 */
 	AdminMenu(String tabName, String tabLink, String elemId) {
 		this.elemId = elemId;
 		this.tabLink = tabLink;
 		this.tabName = tabName;
 	}
 
+	/**
+	 * @see MenuDescriptor
+	 */
 	public String getTabName() {
 		return tabName;
 	}
 
+	/**
+	 * @see MenuDescriptor
+	 */
 	public String getElementId() {
 		return elemId;
 	}
 
+	/**
+	 * @see MenuDescriptor
+	 */
 	public String getTabLink() {
 		return tabLink;
 	}
