8df99a5b86d388b4922e487e44f4b2fcb265ee87
chiara.cerletti@gmail.com <heartripper@users.noreply.github.com>
2012-12-23 09:31:27 +0000

    Aggiunto JavaDoc

---  swim2-cerletti-archetti/SwimWeb/trunk/src/it/polimi/swim/web/pagesupport/ErrorType.java
+++  swim2-cerletti-archetti/SwimWeb/trunk/src/it/polimi/swim/web/pagesupport/ErrorType.java
@@ -1,25 +1,47 @@
 package it.polimi.swim.web.pagesupport;
 
+/**
+ * ErrorType is an enumeration which provides informations about an occurred
+ * error in order to notify it to the user.
+ */
 public enum ErrorType {
 
 	BAD_REQUEST(
 			"Bad Request",
-			"C'è un errore nella tua richiesta. Se hai digitato l'url controlla di non aver fatto errori."),
-	GENERIC(
+			"C'è un errore nella tua richiesta. Se hai digitato l'url controlla di non aver fatto errori."), GENERIC(
 			"Unknown Error",
 			"Sembra che uno degli sviluppatori sia stato pigro nel trattare questo errore...");
 
 	private String errorName, errorDescription;
 
+	/**
+	 * Constructor method.
+	 * 
+	 * @param errorName
+	 *            a String displayed as the name of an error.
+	 * @param errorDescription
+	 *            a String displayed as a description of an error.
+	 */
 	ErrorType(String errorName, String errorDescription) {
 		this.errorName = errorName;
 		this.errorDescription = errorDescription;
 	}
 
+	/**
+	 * Getter method implemented in order to provide the name of an error.
+	 * 
+	 * @return the name of an error.
+	 */
 	public String getErrorName() {
 		return errorName;
 	}
 
+	/**
+	 * Getter method implemented in order to provide the description of an
+	 * error.
+	 * 
+	 * @return the description of an error.
+	 */
 	public String getErrorDescription() {
 		return errorDescription;
 	}
---  swim2-cerletti-archetti/SwimWeb/trunk/src/it/polimi/swim/web/pagesupport/UnloggedMenu.java
+++  swim2-cerletti-archetti/SwimWeb/trunk/src/it/polimi/swim/web/pagesupport/UnloggedMenu.java
@@ -1,26 +1,49 @@
 package it.polimi.swim.web.pagesupport;
 
+/**
+ * UnloggedMenu is an enumeration which implements interface MenuDescriptor in
+ * order to provide informations on the menu displayed to the customer
+ * (unregistered/unlogged user).
+ */
 public enum UnloggedMenu implements MenuDescriptor {
 
-	HOME("Benvenuto", "/", "landing"),
-	SEARCH("Cos'è Swim", "/about/", "about");
+	HOME("Benvenuto", "/", "landing"), SEARCH("Cos'è Swim", "/about/", "about");
 
 	private String tabName, tabLink, elemId;
 
+	/**
+	 * Constructor method.
+	 * 
+	 * @param tabName
+	 *            a String displayed as the name of the tab.
+	 * @param tabLink
+	 *            a String which identifies the path of the link of the tab.
+	 * @param elemId
+	 *            a String which identifies the HTML identifier of the tab.
+	 */
 	UnloggedMenu(String tabName, String tabLink, String elemId) {
 		this.elemId = elemId;
 		this.tabLink = tabLink;
 		this.tabName = tabName;
 	}
 
+	/**
+	 * @see MenuDescriptor
+	 */
 	public String getTabName() {
 		return tabName;
 	}
 
+	/**
+	 * @see MenuDescriptor
+	 */
 	public String getElementId() {
 		return elemId;
 	}
 
+	/**
+	 * @see MenuDescriptor
+	 */
 	public String getTabLink() {
 		return tabLink;
 	}
