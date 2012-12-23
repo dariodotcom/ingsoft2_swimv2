93e7649f943f7d3ab3c460c050d5363f3f5b30ee
chiara.cerletti@gmail.com <chiara.cerletti@gmail.com@458ac278-37c0-ab69-d4d3-e18dd2d1f624>
2012-12-23 09:31:27 +0000

    Aggiunto JavaDoc

---  swim2-cerletti-archetti/SwimWeb/trunk/src/it/polimi/swim/web/pagesupport/CustomerMenu.java
+++  swim2-cerletti-archetti/SwimWeb/trunk/src/it/polimi/swim/web/pagesupport/CustomerMenu.java
@@ -1,29 +1,51 @@
 package it.polimi.swim.web.pagesupport;
 
-public enum CustomerMenu implements MenuDescriptor{
-		
-	HOME("Home", "/home/", "home"),
-	SEARCH("Search", "/search/", "search"),
-	WORKS("Lavori", "/works/", "works"),
-	FEEDBACKS("Feedback", "/feedbacks/", "feedbacks"),
-	FRIENDS("Amici", "/friends/", "friends");
-	
+/**
+ * CustomerMenu is an enumeration which implements interface MenuDescriptor in
+ * order to provide informations on the menu displayed to the customer
+ * (registered user).
+ */
+public enum CustomerMenu implements MenuDescriptor {
+
+	HOME("Home", "/home/", "home"), SEARCH("Search", "/search/", "search"), WORKS(
+			"Lavori", "/works/", "works"), FEEDBACKS("Feedback", "/feedbacks/",
+			"feedbacks"), FRIENDS("Amici", "/friends/", "friends");
+
 	private String tabName, tabLink, elemId;
-	
-	CustomerMenu(String tabName, String tabLink, String elemId){
-		this.elemId =elemId;
+
+	/**
+	 * Constructor method.
+	 * 
+	 * @param tabName
+	 *            a String displayed as the name of the tab.
+	 * @param tabLink
+	 *            a String which identifies the path of the link of the tab.
+	 * @param elemId
+	 *            a String which identifies the HTML identifier of the tab.
+	 */
+	CustomerMenu(String tabName, String tabLink, String elemId) {
+		this.elemId = elemId;
 		this.tabLink = tabLink;
 		this.tabName = tabName;
 	}
 
+	/**
+	 * @see MenuDescriptor
+	 */
 	public String getTabName() {
 		return tabName;
 	}
 
+	/**
+	 * @see MenuDescriptor
+	 */
 	public String getElementId() {
 		return elemId;
 	}
 
+	/**
+	 * @see MenuDescriptor
+	 */
 	public String getTabLink() {
 		return tabLink;
 	}
