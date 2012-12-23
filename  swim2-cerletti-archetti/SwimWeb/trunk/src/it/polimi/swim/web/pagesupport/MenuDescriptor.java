56369a4f152617fb3b68c2d4d04dbafb7819ef15
chiara.cerletti@gmail.com <heartripper@users.noreply.github.com>
2012-12-23 09:08:14 +0000

    Aggiunto JavaDoc

---  swim2-cerletti-archetti/SwimWeb/trunk/src/it/polimi/swim/web/pagesupport/MenuDescriptor.java
+++  swim2-cerletti-archetti/SwimWeb/trunk/src/it/polimi/swim/web/pagesupport/MenuDescriptor.java
@@ -1,7 +1,31 @@
 package it.polimi.swim.web.pagesupport;
 
+/**
+ * MenuDescriptor is an interface which purpose is to retrieve informations
+ * about menu elements.
+ */
 public interface MenuDescriptor {
+	/**
+	 * Getter method implemented in order to provide the visualized name of the
+	 * tab.
+	 * 
+	 * @return the name of the tab in the current page
+	 */
 	public String getTabName();
+
+	/**
+	 * Getter method implemented in order to provide the HTML identifier of the
+	 * tab.
+	 * 
+	 * @return the HTML identifier of the tab.
+	 */
 	public String getElementId();
+
+	/**
+	 * Getter method implemented in order to provide the visualized link of the
+	 * tab.
+	 * 
+	 * @return the link of the tab.
+	 */
 	public String getTabLink();
 }
