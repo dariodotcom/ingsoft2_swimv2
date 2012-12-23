c1dba92ae109f7800da837f040f6c68270af782b
chiara.cerletti@gmail.com <chiara.cerletti@gmail.com@458ac278-37c0-ab69-d4d3-e18dd2d1f624>
2012-12-23 10:36:49 +0000

    Aggiunto JavaDoc

---  swim2-cerletti-archetti/SwimWeb/trunk/src/it/polimi/swim/web/servlets/ServletAction.java
+++  swim2-cerletti-archetti/SwimWeb/trunk/src/it/polimi/swim/web/servlets/ServletAction.java
@@ -6,6 +6,24 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * ServletAction is an interface which purpose is to drive an action.
+ */
 public interface ServletAction {
-	public void runAction(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException;
+	/**
+	 * This method serves to perform an action after an HTTP request.
+	 * 
+	 * @param req
+	 *            an HttpServletRequest which is the HTTP request.
+	 * @param resp
+	 *            an HttpServletRequest which is the response to the HTTP
+	 *            request.
+	 * @throws IOException
+	 *             an exception generated because of a bad input.
+	 * @throws ServletException
+	 *             an exception a servlet can throw when it encounters
+	 *             difficulty.
+	 */
+	public void runAction(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException, ServletException;
 }
