beaec570a17a7fa6deb2ed818bfe10d559371e58
chiara.cerletti@gmail.com <chiara.cerletti@gmail.com@458ac278-37c0-ab69-d4d3-e18dd2d1f624>
2012-12-24 12:48:59 +0000

    Aggiunto JavaDoc

---  swim2-cerletti-archetti/SwimBusiness/trunk/ejbModule/it/polimi/swim/business/entity/Ability.java
+++  swim2-cerletti-archetti/SwimBusiness/trunk/ejbModule/it/polimi/swim/business/entity/Ability.java
@@ -7,10 +7,25 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+/**
+ * Ability is an entity that represents a skill that a user registered to the
+ * platform can claim to have, or, on the opposite side, a profession that you
+ * want to try to establish for working relationship.
+ */
 @Entity
 @Table(name = "ability")
 public class Ability {
 
+	/**
+	 * Class constructor.
+	 * 
+	 * @param name
+	 *            a String which contains the name of an ability.
+	 * @param description
+	 *            a String which contains a brief description of the ability.
+	 * @param acceptor
+	 *            the Administrator that has inserted the ability to the set.
+	 */
 	public Ability(String name, String description, Administrator acceptor) {
 		this.name = name;
 		this.description = description;
@@ -21,34 +36,66 @@ public class Ability {
 	@Column(name = "name")
 	private String name;
 
-	@Column(name = "description", nullable=false)
+	@Column(name = "description", nullable = false)
 	private String description;
 
 	@ManyToOne
-	@JoinColumn(name = "approvedBy", nullable=false)
+	@JoinColumn(name = "approvedBy", nullable = false)
 	private Administrator approvedBy;
 
-	/* Getters and Setters */
+	/**
+	 * Getter method.
+	 * 
+	 * @return a String that contains the ability name.
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Setter method.
+	 * 
+	 * @param name
+	 *            a String that contains the ability name.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Getter method.
+	 * 
+	 * @return a String that contains the ability description.
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * Setter method.
+	 * 
+	 * @param description
+	 *            a String that contains the ability description.
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
+	/**
+	 * Getter method.
+	 * 
+	 * @return the Administrator that has approved the ability.
+	 */
 	public Administrator getApprovedBy() {
 		return approvedBy;
 	}
 
+	/**
+	 * Setter method.
+	 * 
+	 * @param approvedBy
+	 *            the Administrator that has approved the ability.
+	 */
 	public void setApprovedBy(Administrator approvedBy) {
 		this.approvedBy = approvedBy;
 	}
---  swim2-cerletti-archetti/SwimBusiness/trunk/ejbModule/it/polimi/swim/business/entity/AbilityRequest.java
+++  swim2-cerletti-archetti/SwimBusiness/trunk/ejbModule/it/polimi/swim/business/entity/AbilityRequest.java
@@ -8,13 +8,31 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+/**
+ * AbilityRequest is an entity which represents the request to the administrator
+ * by a user to add a skill to the set of professions predefined by the system.
+ */
 @Entity
 @Table(name = "ability_requests")
 public class AbilityRequest {
 
+	/**
+	 * Class constructor.
+	 */
 	public AbilityRequest() {
 	}
 
+	/**
+	 * Class constructor.
+	 * 
+	 * @param author
+	 *            the Customer that has sent the request.
+	 * @param name
+	 *            a String that contains the name of the suggested ability.
+	 * @param description
+	 *            a String that contains a brief description of the suggested
+	 *            ability.
+	 */
 	public AbilityRequest(Customer author, String name, String description) {
 		this.abilityName = name;
 		this.abilityDescription = description;
@@ -42,47 +60,113 @@ public class AbilityRequest {
 	@JoinColumn(name = "requestauthor")
 	private Customer requestAuthor;
 
-	/* Getters and Setters */
+	/**
+	 * Getter method.
+	 * 
+	 * @return a String that contains the name given to the suggested ability.
+	 */
 	public String getAbilityName() {
 		return abilityName;
 	}
 
+	/**
+	 * Setter method.
+	 * 
+	 * @param abilityName
+	 *            a String that contains the name given to the suggested
+	 *            ability.
+	 */
 	public void setAbilityName(String abilityName) {
 		this.abilityName = abilityName;
 	}
 
+	/**
+	 * Getter method.
+	 * 
+	 * @return a String that contains the description given to the suggested
+	 *         ability.
+	 */
 	public String getAbilityDescription() {
 		return abilityDescription;
 	}
 
+	/**
+	 * Setter method.
+	 * 
+	 * @param abilityDescription
+	 *            a String that contains the description given to the suggested
+	 *            ability.
+	 */
 	public void setAbilityDescription(String abilityDescription) {
 		this.abilityDescription = abilityDescription;
 	}
 
+	/**
+	 * Getter method.
+	 * 
+	 * @return a Boolean that is true if the ability request has been approved,
+	 *         false otherwise.
+	 */
 	public Boolean getApproved() {
 		return approved;
 	}
 
+	/**
+	 * Setter method.
+	 * 
+	 * @param approved
+	 *            a Boolean that is true if the ability request has been
+	 *            approved, false otherwise.
+	 */
 	public void setApproved(Boolean approved) {
 		this.approved = approved;
 	}
 
+	/**
+	 * Getter method.
+	 * 
+	 * @return a String that contains the review given by the administrator for
+	 *         the ability request.
+	 */
 	public String getReview() {
 		return review;
 	}
 
+	/**
+	 * Setter method.
+	 * 
+	 * @param review
+	 *            a String that contains the review given by the administrator
+	 *            for the ability request.
+	 */
 	public void setReview(String review) {
 		this.review = review;
 	}
 
+	/**
+	 * Getter method.
+	 * 
+	 * @return the Customer that has sent the request.
+	 */
 	public Customer getRequestAuthor() {
 		return requestAuthor;
 	}
 
+	/**
+	 * Setter method.
+	 * 
+	 * @param requestAuthor
+	 *            the Customer that has sent the request.
+	 */
 	public void setRequestAuthor(Customer requestAuthor) {
 		this.requestAuthor = requestAuthor;
 	}
 
+	/**
+	 * Getter method.
+	 * 
+	 * @return an Integer that represents the identificator of the request.
+	 */
 	public int getId() {
 		return id;
 	}
